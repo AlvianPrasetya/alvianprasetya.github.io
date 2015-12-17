952d2d25bebd2eef97a1c220798c97a4bae7e7be
Alvian Prasetya <alvian.better@gmail.com>
2015-12-17 20:11:14 +0800

    Minified javascripts and css files

---
+++ assets/js/parallax.min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$window=$(window),$('section[data-type="background"]').each(function(){var n=$(this);$(window).scroll(function(){var o=-($window.scrollTop()/n.data("speed")),c="50% "+o+"px";n.css({backgroundPosition:c})})})}),document.createElement("section");
